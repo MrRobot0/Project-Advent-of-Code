05e78372ce257c2d3410f96152033ba42d171ffa
Wietze van Osch <wietze@osch.net>
2022-12-08 15:12:21 +0100

    Added some debugging

--- 2022/7/7.1.py
+++ 2022/7/7.1.py
@@ -1,8 +1,10 @@
 from Folder import Folder
+from File import File
 
-f = open("7-input.txt")
+f = open("D:\OneDrive\OneDrive - Office 365 Fontys\______________\Projects\Project Advent of Code\\2022\\7\\7-input.txt")
 maxDirSize = 100000
 dirSum = 0
+debug = False
             
 rootDirectory = Folder(name="/", rootDir=None)
 currentDir = rootDirectory
@@ -12,28 +14,31 @@ def checkDirSize(folder = Folder()):
     if folder.size <= maxDirSize:
         dirSum += folder.size
 
+print("--------={ BEGIN }=--------")
 for line in f:
     if line[0] == "$":
         line = line.rstrip().split(" ")
         if line[1] == "cd":
             dir = line[2]
+            if dir == "/":
+                currentDir = rootDirectory
+                continue
             if dir == "..":
-                currentDir = currentDir.rootDir
+                if currentDir.rootDir != None:
+                    currentDir = currentDir.rootDir
             else:
                 for f in currentDir.subdirs:
                     if f.name == dir:
                         currentDir = f
+                        break
     else:
         line = line.rstrip().split(" ")
         if line[0] == "dir":
             dir = line[1]
-            currentDir.addSubDir(dir)
+            currentDir.addSubDir(dir, debug=debug)
         else:
             size = int(line[0])
             name = line[1]
-            currentDir.addFile(name=name,size=size)
-
-
-
+            currentDir.addFile(name=name,size=size, debug=debug)
 
 print(dirSum)
--- 2022/7/Folder.py
+++ 2022/7/Folder.py
@@ -7,19 +7,27 @@ class Folder:
         self.subdirs = subdirs
         self.rootDir = rootDir
         self.files = files
-    def addSubDir(self, name = ""):
+    def addSubDir(self, name = "", debug = False):
+        if debug:
+            print(f"Adding {name} (dir) to {self.name}")
         self.subdirs.append(Folder(rootDir=self, name=name))
-    def addFile(self, name = "", size = 0):
+    def addFile(self, name = "", size = 0, debug = False):
+        if debug:
+             print(f"Adding {name} (file) to {self.name}")
         self.files.append(File(name, size))
         self.size += size
         self.calcSize()
+    def ls(self):
+        content = self.subdirs.copy()
+        content.extend(self.files)
+        return content
     def changeSize(self, size = 0):
         if self.rootDir != None:
             self.size = size
             self.rootDir.calcSize()
     def calcSize(self):
         self.size = 0
-        for dir in self.subdirs:
+        for dir in self.subdirs: 
             self.size += dir.size
         for file in self.files:
             self.size += file.size
